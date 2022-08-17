d98be1452214ec0985cd7fc15e89e41952fcb8d9
lostred <lostred@outlook.com>
2022-08-17 09:56:35 +0800

    新增抽象规则类

--- ruler-core/src/main/java/info/lostred/ruler/rule/GenericDomainRule.java
+++ ruler-core/src/main/java/info/lostred/ruler/rule/GenericDomainRule.java
@@ -1,11 +1,11 @@
 package info.lostred.ruler.rule;
 
-import info.lostred.ruler.core.GenericCollector;
-import info.lostred.ruler.core.GenericJudgement;
 import info.lostred.ruler.domain.RuleDefinition;
 import org.springframework.expression.EvaluationContext;
 import org.springframework.expression.ExpressionParser;
 
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,15 +14,19 @@ import java.util.Map;
  *
  * @param <T> 领域模型类型
  */
-public abstract class GenericDomainRule<T> extends AbstractRule implements GenericJudgement<T>, GenericCollector<T> {
-    private final Class<T> domainClass;
+@SuppressWarnings("unchecked")
+public abstract class GenericDomainRule<T> extends AbstractRule {
+    private Class<T> domainClass;
 
-    public GenericDomainRule(RuleDefinition ruleDefinition, Class<T> domainClass) {
+    public GenericDomainRule(RuleDefinition ruleDefinition) {
         super(ruleDefinition);
-        this.domainClass = domainClass;
+        Type type = this.getClass().getGenericSuperclass();
+        if (type instanceof ParameterizedType) {
+            Type[] types = ((ParameterizedType) type).getActualTypeArguments();
+            this.domainClass = (Class<T>) types[0];
+        }
     }
 
-    @SuppressWarnings("unchecked")
     @Override
     public boolean supports(EvaluationContext context, ExpressionParser parser, Object object) {
         if (domainClass.isAssignableFrom(object.getClass())) {
@@ -31,7 +35,6 @@ public abstract class GenericDomainRule<T> extends AbstractRule implements Gener
         return false;
     }
 
-    @SuppressWarnings("unchecked")
     @Override
     public boolean judge(EvaluationContext context, ExpressionParser parser, Object object) {
         if (domainClass.isAssignableFrom(object.getClass())) {
@@ -40,7 +43,6 @@ public abstract class GenericDomainRule<T> extends AbstractRule implements Gener
         return false;
     }
 
-    @SuppressWarnings("unchecked")
     @Override
     public Map<String, Object> collectMappings(EvaluationContext context, ExpressionParser parser, Object object) {
         if (domainClass.isAssignableFrom(object.getClass())) {
@@ -48,4 +50,28 @@ public abstract class GenericDomainRule<T> extends AbstractRule implements Gener
         }
         return new HashMap<>(0);
     }
+
+    /**
+     * 是否支持对该领域实例进行判断
+     *
+     * @param object 领域实例
+     * @return 支持返回true，否则返回false
+     */
+    protected abstract boolean supports(T object);
+
+    /**
+     * 判断领域实例是否满足特定的条件
+     *
+     * @param object 领域实例
+     * @return 满足条件返回true，否则返回false
+     */
+    protected abstract boolean judge(T object);
+
+    /**
+     * 收集入参object中字段与值的映射关系
+     *
+     * @param object 领域实例
+     * @return 违规字段与值的映射关系集合
+     */
+    protected abstract Map<String, Object> collectMappings(T object);
 }
---
+++ ruler-core/src/main/java/info/lostred/ruler/rule/SpELRule.java
@@ -0,0 +1,51 @@
+package info.lostred.ruler.rule;
+
+import info.lostred.ruler.domain.RuleDefinition;
+import org.springframework.expression.EvaluationContext;
+import org.springframework.expression.ExpressionParser;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static info.lostred.ruler.constant.SpELConstants.INDEX;
+import static info.lostred.ruler.constant.SpELConstants.INDEX_LABEL;
+
+/**
+ * SpEL规则
+ *
+ * @author lostred
+ */
+public abstract class SpELRule extends AbstractRule {
+    public SpELRule(RuleDefinition ruleDefinition) {
+        super(ruleDefinition);
+    }
+
+    @Override
+    public boolean supports(EvaluationContext context, ExpressionParser parser, Object object) {
+        String conditionExp = ruleDefinition.getConditionExp();
+        Boolean flag = parser.parseExpression(conditionExp).getValue(context, Boolean.class);
+        return Boolean.TRUE.equals(flag);
+    }
+
+    @Override
+    public boolean judge(EvaluationContext context, ExpressionParser parser, Object object) {
+        String predicateExp = ruleDefinition.getPredicateExp();
+        Boolean flag = parser.parseExpression(predicateExp).getValue(context, Boolean.class);
+        return Boolean.TRUE.equals(flag);
+    }
+
+    @Override
+    public Map<String, Object> collectMappings(EvaluationContext context, ExpressionParser parser, Object object) {
+        Map<String, Object> map = new HashMap<>();
+        String parameterExp = ruleDefinition.getParameterExp();
+        Object value = parser.parseExpression(parameterExp).getValue(context);
+        if (parameterExp.contains(INDEX_LABEL)) {
+            Object index = parser.parseExpression(INDEX).getValue(context);
+            assert index != null;
+            map.put(parameterExp.replace(INDEX, index.toString()), value);
+        } else {
+            map.put(parameterExp, value);
+        }
+        return map;
+    }
+}
