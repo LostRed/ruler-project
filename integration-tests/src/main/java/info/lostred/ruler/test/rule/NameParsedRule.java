79dcae326adf4f06d1ef01ae62e78089553ddbd6
lostred <lostred@outlook.com>
2022-11-30 12:07:18 +0800

    重新定义AbstractRule的子类，修改相关的注释与文档

--- integration-tests/src/main/java/info/lostred/ruler/test/rule/NameParsedRule.java
+++ integration-tests/src/main/java/info/lostred/ruler/test/rule/NameParsedRule.java
@@ -2,15 +2,20 @@ package info.lostred.ruler.test.rule;
 
 import info.lostred.ruler.annotation.Rule;
 import info.lostred.ruler.domain.RuleDefinition;
-import info.lostred.ruler.rule.GenericRule;
+import info.lostred.ruler.rule.ProgrammaticRule;
 
 @Rule(ruleCode = "姓名必填",
         businessType = "person",
         description = "姓名不能为空",
         parameterExp = "name")
-public class NameRule extends GenericRule<String> {
-    public NameRule(RuleDefinition ruleDefinition) {
+public class NameParsedRule extends ProgrammaticRule<String> {
+    public NameParsedRule(RuleDefinition ruleDefinition) {
         super(ruleDefinition);
+    }
+
+    @Override
+    protected boolean doSupports(String value) {
+        return true;
     }
 
     @Override
