c0d4c913f7dc94c204a4911b8762d95353531300
lostred <lostred@outlook.com>
2022-12-01 11:11:55 +0800

    修改测试接口

--- integration-tests/src/main/java/info/lostred/ruler/test/controller/TestController.java
+++ integration-tests/src/main/java/info/lostred/ruler/test/controller/TestController.java
@@ -1,6 +1,11 @@
 package info.lostred.ruler.test.controller;
 
+import info.lostred.ruler.domain.Result;
 import info.lostred.ruler.engine.RulesEngine;
+import info.lostred.ruler.test.domain.Person;
+import org.springframework.expression.EvaluationContext;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,8 +17,10 @@ public class TestController {
         this.rulesEngine = rulesEngine;
     }
 
-//    @PostMapping("/execute")
-//    public Result execute(@RequestBody Person person) {
-//        return rulesEngine.execute(person);
-//    }
+    @PostMapping("/execute")
+    public Result execute(@RequestBody Person person) {
+        EvaluationContext context = rulesEngine.createEvaluationContext(person);
+        rulesEngine.execute(context);
+        return rulesEngine.getResult(context);
+    }
 }
